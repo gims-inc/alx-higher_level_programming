4d8eb27acbd63c8e70e6350f72b846841133248a
gims-inc <gabrielmusanga@gmail.com>
2022-09-27 16:27:05 -0700

    To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""String to JSON function"""
+import json
+
+
+
+def to_json_string(my_obj):
+    """Return a JSON representation of a string object."""
+    return json.dumps(my_obj)
