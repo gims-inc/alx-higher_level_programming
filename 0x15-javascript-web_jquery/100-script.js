10971280fce09bd218045028bf736b065a8ee09c
gims-inc <gabrielmusanga@gmail.com>
2023-01-26 18:22:47 +0300

    fix

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,4 +1,4 @@
-addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
   const redHeader = document.querySelector('header');
   redHeader.style.color = '#FF0000';
 });
